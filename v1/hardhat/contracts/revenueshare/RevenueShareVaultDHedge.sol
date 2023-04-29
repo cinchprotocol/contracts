a28ca11d73bf9ecf4630939d3417c9bb9bd26c66
Sing <16409791+singyiu@users.noreply.github.com>
2023-04-29 14:36:57 -0700

    reordered RevenueShareVaultDHedge.sol

--- v1/hardhat/contracts/revenueshare/RevenueShareVaultDHedge.sol
+++ v1/hardhat/contracts/revenueshare/RevenueShareVaultDHedge.sol
@@ -30,6 +30,63 @@ contract RevenueShareVaultDHedge is RevenueShareVault {
     }
 
     /**
+     * @notice Redeem assets with vault shares and referral
+     * @dev whenNotPaused
+     * @dev nonReentrant
+     * @dev if _msgSender() != sharesOwner, then the sharesOwner must have approved this contract to spend the shares (checked inside the _withdraw call)
+     * @param shares amount of shares to burn and redeem assets
+     * @param receiver address to receive the assets
+     * @param sharesOwner address of the owner of the shares to be consumed, require to be _msgSender() for better security
+     * @param referral address of the partner referral
+     * @param expectedAmountOut expected amount of assets to be received (slippage protection)
+     * @return amount of assets received
+     */
+    function redeemWithReferralAndExpectedAmountOut(uint256 shares, address receiver, address sharesOwner, address referral, uint256 expectedAmountOut) external virtual whenNotPaused nonReentrant returns (uint256) {
+        require(shares > 0, "ZERO_SHARES");
+        require(receiver != address(0) && referral != address(0), "ZERO_ADDRESS");
+        require(shares <= maxRedeem(sharesOwner), "RevenueShareVault: max redeem exceeded");
+        require(shares <= totalSharesByUserReferral[sharesOwner][referral], "RevenueShareVault: insufficient shares by referral");
+
+        //remove the shares from the user record first to avoid reentrancy attack
+        _trackSharesInReferralRemoved(sharesOwner, referral, shares);
+
+        uint256 assets = _redeemFromYieldSourceVault(shares, expectedAmountOut);
+        _redeem(_msgSender(), receiver, sharesOwner, assets, shares);
+        emit RedeemWithReferral(_msgSender(), receiver, sharesOwner, assets, shares, referral);
+        return assets;
+    }
+
+    /**
+     * @dev to be used for calculating the revenue share ratio
+     * @return yieldSourceTotalShares total yield source shares supply
+     */
+    function getYieldSourceVaultTotalShares() external view override returns (uint256) {
+        return IYieldSourceDHedge(yieldSourceVault).totalSupply();
+    }
+
+    /**
+     * @notice Redeem assets with vault shares and referral
+     * @dev For this integration, redeemWithReferralAndExpectedAmountOut is supported instead of redeemWithReferral
+     * @dev not supported
+     * shares amount of shares to burn and redeem assets
+     * receiver address to receive the assets
+     * sharesOwner address of the owner of the shares to be consumed, require to be _msgSender() for better security
+     * referral address of the partner referral
+     * @return assets_ amount of assets received
+     */
+    function redeemWithReferral(uint256, address, address, address) public pure override returns (uint256) {
+        require(false, "RevenueShareVaultDHedge: not supported");
+    }
+
+    /**
+     * @param account target account address
+     * @return shares yield source share balance of this vault
+     */
+    function shareBalanceAtYieldSourceOf(address account) public view override returns (uint256) {
+        return IYieldSourceDHedge(yieldSourceVault).balanceOf(account);
+    }
+
+    /**
      * @dev Deposit assets to yield source vault
      * @dev virtual, expected to be overridden with specific yield source vault
      * @param asset_ The address of the ERC20 asset contract
@@ -65,63 +122,6 @@ contract RevenueShareVaultDHedge is RevenueShareVault {
         IYieldSourceDHedgeSwapper(yieldSourceSwapper).withdraw(yieldSourceVault, shares, IERC20(asset), expectedAmountOut);
         uint256 assetBalance1 = IERC20(asset).balanceOf(address(this));
         return assetBalance1 - assetBalance0;
-    }
-
-    /**
-     * @notice Redeem assets with vault shares and referral
-     * @dev whenNotPaused
-     * @dev nonReentrant
-     * @dev if _msgSender() != sharesOwner, then the sharesOwner must have approved this contract to spend the shares (checked inside the _withdraw call)
-     * @param shares amount of shares to burn and redeem assets
-     * @param receiver address to receive the assets
-     * @param sharesOwner address of the owner of the shares to be consumed, require to be _msgSender() for better security
-     * @param referral address of the partner referral
-     * @param expectedAmountOut expected amount of assets to be received (slippage protection)
-     * @return amount of assets received
-     */
-    function redeemWithReferralAndExpectedAmountOut(uint256 shares, address receiver, address sharesOwner, address referral, uint256 expectedAmountOut) public virtual whenNotPaused nonReentrant returns (uint256) {
-        require(shares > 0, "ZERO_SHARES");
-        require(receiver != address(0) && referral != address(0), "ZERO_ADDRESS");
-        require(shares <= maxRedeem(sharesOwner), "RevenueShareVault: max redeem exceeded");
-        require(shares <= totalSharesByUserReferral[sharesOwner][referral], "RevenueShareVault: insufficient shares by referral");
-
-        //remove the shares from the user record first to avoid reentrancy attack
-        _trackSharesInReferralRemoved(sharesOwner, referral, shares);
-
-        uint256 assets = _redeemFromYieldSourceVault(shares, expectedAmountOut);
-        _redeem(_msgSender(), receiver, sharesOwner, assets, shares);
-        emit RedeemWithReferral(_msgSender(), receiver, sharesOwner, assets, shares, referral);
-        return assets;
-    }
-
-    /**
-     * @notice Redeem assets with vault shares and referral
-     * @dev For this integration, redeemWithReferralAndExpectedAmountOut is supported instead of redeemWithReferral
-     * @dev not supported
-     * shares amount of shares to burn and redeem assets
-     * receiver address to receive the assets
-     * sharesOwner address of the owner of the shares to be consumed, require to be _msgSender() for better security
-     * referral address of the partner referral
-     * @return assets_ amount of assets received
-     */
-    function redeemWithReferral(uint256, address, address, address) public pure override returns (uint256) {
-        require(false, "RevenueShareVaultDHedge: not supported");
-    }
-
-    /**
-     * @param account target account address
-     * @return shares yield source share balance of this vault
-     */
-    function shareBalanceAtYieldSourceOf(address account) public view override returns (uint256) {
-        return IYieldSourceDHedge(yieldSourceVault).balanceOf(account);
-    }
-
-    /**
-     * @dev to be used for calculating the revenue share ratio
-     * @return yieldSourceTotalShares total yield source shares supply
-     */
-    function getYieldSourceVaultTotalShares() external view override returns (uint256) {
-        return IYieldSourceDHedge(yieldSourceVault).totalSupply();
     }
 
     /**
