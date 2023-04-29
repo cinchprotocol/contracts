880b89fa07f90524b754ba5891f6d0e0b7a0e5e1
Sing <16409791+singyiu@users.noreply.github.com>
2023-04-29 08:17:23 -0700

    pragma solidity 0.8.19

--- v1/hardhat/contracts/revenueshare/security/DepositPausableUpgradeable.sol
+++ v1/hardhat/contracts/revenueshare/security/DepositPausableUpgradeable.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v4.7.0) (security/Pausable.sol)
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
